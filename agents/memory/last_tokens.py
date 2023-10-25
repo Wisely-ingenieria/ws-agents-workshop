73c704a54e2834f05940c5244eda08e613d23bfe
HernanOlave <hernan.olave.b@gmail.com>
2023-10-25 12:04:56 -0300

    cleanup and bug fixes

--- agents/memory/last_tokens.py
+++ agents/memory/last_tokens.py
@@ -1,42 +1,46 @@
-from datetime import datetime
 from llm import count_tokens
 from utils import Logger
+from .last_memories import LastMemories
 
-class LastTokens:
-    def __init__(self, max_tokens=2000):            
-        self.memory = []
+class LastTokens(LastMemories):
+    def __init__(self):            
+        super().__init__()
         self.log = Logger()
 
     def add_to_memory(self, role, content):
-        memory_item = {
-            "timestamp": datetime.now(),
-            "token_count": count_tokens(content),
-            "role": role,
-            "content": content
-        }
-        self.memory.append(memory_item)
+        super().add_to_memory(role, content)
+        memory_item = self.memory[-1]
+        memory_item["token_count"] = count_tokens(content)
         self.log_memory_update(memory_item)
 
     def log_memory_update(self, memory_item):
-        total_memory_tokens = sum([mem["token_count"] for mem in self.memory])
+        total_memory_tokens = sum([mem.get("token_count", 0) for mem in self.memory])
         self.log.info(
             f"Adding memory item {len(self.memory)} with {memory_item['token_count']} tokens. "
             f"Total memory tokens: {total_memory_tokens}"
         )
     
     def get_last_tokens(self, max_tokens=2000):
-        total_memory_tokens = sum([mem["token_count"] for mem in self.memory])
+        total_memory_tokens = sum([mem.get("token_count", 0) for mem in self.memory])
         if total_memory_tokens < max_tokens:
-            return [{"role": memory["role"], "content": memory["content"]} for memory in self.memory]
+            return [
+                {
+                    "role": memory["role"], 
+                    "content": memory["content"],
+                    "timestamp" : memory["timestamp"]
+                } for memory in self.memory][::-1]
 
-        last_tokens = []
+        _messages = []
+        _tokens = 0
         for memory in reversed(self.memory):
-            last_tokens.append({
-                "role": memory["role"],
-                "content": memory["content"]
-            })
-            total_memory_tokens -= memory["token_count"]
-            if total_memory_tokens < max_tokens:
+            _tokens += memory.get("token_count", 0)
+            if _tokens >= max_tokens:
                 break
-
-        return last_tokens
+            
+            _messages.append({
+                "role": memory["role"],
+                "content": memory["content"],
+                "timestamp" : memory["timestamp"]
+            })
+            
+        return _messages[::-1]
--- agents/memory/relevant_memories.py
+++ agents/memory/relevant_memories.py
@@ -1,34 +1,25 @@
-from datetime import datetime
-from llm import generate_embeddings, count_tokens
-from utils import Logger, vector_similarity
+from llm import generate_embeddings
+from utils import  vector_similarity
+from .last_tokens import LastTokens
 
-class RelevantMemories:
+class RelevantMemories(LastTokens):
     def __init__(self):            
-        self.memory = []
-        self.log = Logger()        
+        super().__init__()
 
     def add_to_memory(self, role, content):
-        memory_item = {
-            "timestamp": datetime.now(),
-            "token_count": count_tokens(content),
-            "role": role,
-            "content": content,
-            "embedding": generate_embeddings(content)
-        }
-        self.memory.append(memory_item)
+        super().add_to_memory(role, content)
+        memory_item = self.memory[-1]
+        memory_item["embedding"] = generate_embeddings(content)
         self.log_memory_update(memory_item)
-
-    def log_memory_update(self, memory_item):
-        total_memory_tokens = sum([mem["token_count"] for mem in self.memory])
-        self.log.info(
-            f"Adding memory item {len(self.memory)} with {memory_item['token_count']} tokens. "
-            f"Total memory tokens: {total_memory_tokens}"
-        )
 
     def get_relevant_memories(self, query, max_tokens = 2000, sim_k = 0.8, rec_k = 0.25):
         total_memory_tokens = sum([memory_item["token_count"] for memory_item in self.memory])
         if total_memory_tokens < max_tokens:
-            return [{"role": memory["role"], "content": memory["content"]} for memory in self.memory]
+            return [{
+                    "role": memory["role"], 
+                    "content": memory["content"], 
+                    "timestamp": memory["timestamp"]} 
+                for memory in self.memory]
 
         self.log.info(f"Executing get_relevant_memories with the following parameters: max_tokens: {max_tokens}, sim_k: {sim_k}, rec_k: {rec_k}, Query: {query}")
 
@@ -40,7 +31,7 @@ class RelevantMemories:
             recency = memory_index / len(self.memory)
             score = (sim_k * similarity) + (rec_k * recency)
 
-            self.log.info(f"Calculating score for memory {memory_index}: Similarity: {similarity} - Recency: {recency} => Score: {score}")
+            self.log.info(f"Calculating score for memory {memory_index}: Similarity: {similarity:.2f} - Recency: {recency:.2f} => Score: {score:.2f}")
 
             relevant_memories.append({
                 "content": memory["content"],
@@ -59,14 +50,13 @@ class RelevantMemories:
             self.log.info(f"Removed memory with {removed_memory['token_count']} tokens.")
 
         relevant_memories.sort(key=lambda x: x["timestamp"], reverse=False)
-        self.log.info("[RELEVANT MEMORIES]")
 
         relevant_messages = []
-
         for memory in relevant_memories:
             relevant_messages.append({
                 "role" : memory["role"],
-                "content": memory["content"]
+                "content": memory["content"],
+                "timestamp": memory["timestamp"]
             })
 
         return relevant_messages
